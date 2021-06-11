ef7f3ae8e6c24d3d83e8a712804cb4238e611aad
Daniel Kirwan <danielkirwan07@gmail.com>
2021-06-11 14:59:03 +0100

    point and click player movement

---
+++ Stealth Game/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    NavMeshAgent player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if left click
+        //get mouse hit position using a raycast
+        //debug hit position
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Hit: " + hit.point);
+                //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //cube.transform.position = hit.point;
+                player.SetDestination(hit.point);
+            }
+        }
+
+    }
+
+
+
+
+
+}
